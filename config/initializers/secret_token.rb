1b3513514da11905618a8a6ae08ddf23a9adb8f7
Jiahao Li <isundaylee.reg@gmail.com>
2013-10-06 12:28:07 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roses::Application.config.secret_key_base = 'eaced413b617da2b33cfca9d8a6fe254cbc496d29dc98a4fa999cf7f7e978e94917bcb31766a512745f1d8225b3fdb1d00199aabe87a515c0150df2da9bcc18d'
